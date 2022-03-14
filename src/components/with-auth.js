e64c3eb37d52f0dc27a083d221dd2d671803e338
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-03-14 02:14:25 -0700

    fixing withAuth error in production

--- src/components/with-auth.js
+++ src/components/with-auth.js
@@ -2,9 +2,11 @@ import { storage } from "../utils";
 import { DashboardLayout } from "./dashboard/dashboard-layout";
 
 import Login from "../pages/login.js";
+import { useAuth } from "../lib/auth";
 
 const withAuth = (Component, isLoggedIn) => {
   const Auth = () => {
+    const { user, login, refetchUser } = useAuth();
     if (!storage.getToken()) return <Login />;
 
     return (
